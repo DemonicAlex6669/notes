33a165601a39e3d1d6d40bfa5badf5c9aa977945
alex watson <alexwatsondemon@gmail.com>
2025-02-24 10:52:40 -0800

    layout complete

--- notes.py
+++ notes.py
@@ -1,20 +1,24 @@
-class Note:
-    def __init(self, ...):
-        self._title = ...
-        self._tags = ...
-        self._note = ...
-    def ...(self, ...):
-        ...
+import pandas as pd
 
 
 def main():
-    choice = input("what do you want to do")
+    df = pd.read_csv("notes.csv")
+    choice = input("what do you want to do").strip().lower()
     if choice == "write":
-        ...
+        title = input("title:")
+        note = input("note:")
+        tags = input("tags")
+        newline = [[title, note, tag]]
+        df2 = pandas.DataFrame(newline)
+        df2.to_csv("notes.csv", index=false, mode="a", headers=false)
     elif choice == "search":
-        ...
+        print(df.loc[choice in df["tags"]])
+    elif choice == "tags":
+        print(df.loc[:, "tags"])
+    elif choice == "options":
+        print("write, search, tags, options")
     else:
-        ...
+        print("incorrect input, for help type options")
 
 
 if __name__ == "__main__":
