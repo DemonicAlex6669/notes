686d30906bb4cbfb4544be90e413d5852eab2ee2
alex watson <alexwatsondemon@gmail.com>
2025-05-20 20:17:15 -0700

    1.0 finish, fixed search, added note format

--- notes.py
+++ notes.py
@@ -4,6 +4,19 @@ import pandas as pd
 def main():
     df = pd.read_csv("notes.csv")
 
+    def format(title, note, tags):
+        title_length = len(title)
+        print(f"#" * title_length)
+        print(f"# {title} #")
+
+        print(f"#" * 25)
+        print(f"# {note} #")
+        print(f"#" * 25)
+
+        tag_length = len(tags)
+        print(f"# {tags} #")
+        print(f"#" * tag_length)
+
     
     while True:
         choice = input("what do you want to do: ").strip().lower()
@@ -18,7 +31,14 @@ def main():
 
         elif choice == "search":
             item = input("search: ")
-            print(df[df["tags"].str.contains(item, case=False)])
+            cpdf = df[df["tags"].str.contains(item, case=False) | df["title"].str.contains(item, case=False) | df["note"].str.contains(item, case=False)]
+            format(cpdf.iloc[0,0], cpdf.iloc[0,1], cpdf.iloc[0,2])
+
+            i = len(cpdf) - 1
+
+            while i > 0:
+                format(cpdf.iloc[i,0], cpdf.iloc[i,1], cpdf.iloc[i,2])
+                i = i - 1
 
         elif choice == "tags":
             print(df.loc[:, "tags"])
