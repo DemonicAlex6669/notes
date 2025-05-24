720c330b1fb743e2c34952a4d7b365304939521c
alex watson <alexwatsondemon@gmail.com>
2025-05-24 14:31:26 -0700

    documenting fix

--- notes.py
+++ notes.py
@@ -4,11 +4,12 @@ import pandas as pd
 def main():
     df = pd.read_csv("notes.csv")
 
-    def format(title: str, note: str, tags: str) -> "output string":
+    def format(title: str, note: str, tags: str) -> "string":
         """format row from dataframe into stylized text
 
-        input is dataframe{i,0] [i,1] [i,2] where i is the row selected before input to format
-        output is a styleized string"""
+        input: dataframe{i,0] [i,1] [i,2], where i is the row selected before input to format
+
+        output: styleized string"""
         title_length: int = len(title)
         print(f"#" * title_length)
         print(f"# {title} #")
@@ -25,7 +26,7 @@ def main():
     while True:
         choice: str = input("what do you want to do: ").strip().lower()
     
-        if choice: str == "write":
+        if choice == "write":
             title: str = input("title:")
             note: str = input("note:")
             tags: str = input("tags")
@@ -33,7 +34,7 @@ def main():
             df2 = pd.DataFrame(newline)
             df2.to_csv("notes.csv", index=False, mode="a", header=False)
 
-        elif choice: str == "search":
+        elif choice == "search":
             item: str = input("search: ")
             cpdf = df[df["tags"].str.contains(item, case=False) | df["title"].str.contains(item, case=False) | df["note"].str.contains(item, case=False)]
             format(cpdf.iloc[0,0], cpdf.iloc[0,1], cpdf.iloc[0,2])
@@ -44,13 +45,13 @@ def main():
                 format(cpdf.iloc[i,0], cpdf.iloc[i,1], cpdf.iloc[i,2])
                 i = i - 1
 
-        elif choice: str == "tags":
+        elif choice == "tags":
             print(df.loc[:, "tags"])
 
-        elif choice: str == "options":
+        elif choice == "options":
             print("write, search, tags, options, exit")
 
-        elif choice: str == "exit":
+        elif choice == "exit":
             break
 
         else:
