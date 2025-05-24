4091bd079d32c96980e7564236468bf1753f840d
alex watson <alexwatsondemon@gmail.com>
2025-05-24 14:09:43 -0700

    added documentation

--- notes.py
+++ notes.py
@@ -4,8 +4,8 @@ import pandas as pd
 def main():
     df = pd.read_csv("notes.csv")
 
-    def format(title, note, tags):
-        title_length = len(title)
+    def format(title: str, note: str, tags: str) -> "output string":
+        title_length: int = len(title)
         print(f"#" * title_length)
         print(f"# {title} #")
 
@@ -13,40 +13,40 @@ def main():
         print(f"# {note} #")
         print(f"#" * 25)
 
-        tag_length = len(tags)
+        tag_length: int = len(tags)
         print(f"# {tags} #")
         print(f"#" * tag_length)
 
     
     while True:
-        choice = input("what do you want to do: ").strip().lower()
+        choice: str = input("what do you want to do: ").strip().lower()
     
-        if choice == "write":
-            title = input("title:")
-            note = input("note:")
-            tags = input("tags")
-            newline = [[title, note, tags]]
+        if choice: str == "write":
+            title: str = input("title:")
+            note: str = input("note:")
+            tags: str = input("tags")
+            newline: list = [[title, note, tags]]
             df2 = pd.DataFrame(newline)
             df2.to_csv("notes.csv", index=False, mode="a", header=False)
 
-        elif choice == "search":
-            item = input("search: ")
+        elif choice: str == "search":
+            item: str = input("search: ")
             cpdf = df[df["tags"].str.contains(item, case=False) | df["title"].str.contains(item, case=False) | df["note"].str.contains(item, case=False)]
             format(cpdf.iloc[0,0], cpdf.iloc[0,1], cpdf.iloc[0,2])
 
-            i = len(cpdf) - 1
+            i: int = len(cpdf) - 1
 
             while i > 0:
                 format(cpdf.iloc[i,0], cpdf.iloc[i,1], cpdf.iloc[i,2])
                 i = i - 1
 
-        elif choice == "tags":
+        elif choice: str == "tags":
             print(df.loc[:, "tags"])
 
-        elif choice == "options":
+        elif choice: str == "options":
             print("write, search, tags, options, exit")
 
-        elif choice == "exit":
+        elif choice: str == "exit":
             break
 
         else:
